ff59537f90f11aec9158155ebef2261bb848e41f
Ganderman-Dan <46117179+Ganderman-Dan@users.noreply.github.com>
2020-05-14 19:59:04 -0400

    Small work on waypoint

--- Gander Horror/Assets/Scripts/WaypointNavigation.cs
+++ Gander Horror/Assets/Scripts/WaypointNavigation.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class WaypointNavigation : MonoBehaviour
 {
+    public Vector3 currentDestination;
 
-
+    private Rigidbody rb;
     
-    // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        Movement();
+    }
 
+    private void Movement()
+    {
+
+    }
+
+    //stop and reconsider movement ? wait and go back to path : WALK to a node nearby the noise heard
+    //For when player makes minor noise
+    public void ReconsiderMovement(Node destination)
+    {
+
+    }
+
+    //When player makes a loud noise forget current destination and RUN to the node nearest the noise made, starts walking when he reaches point
+    //-very brief pause tho
+    public void HurryToPlayer(Node Destination)
+    {
+
+    }
 }
 
