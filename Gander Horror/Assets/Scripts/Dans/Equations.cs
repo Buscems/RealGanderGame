0dcece8e73e589743af99ab0d536ce2c03fa35fd
Ganderman-Dan <46117179+Ganderman-Dan@users.noreply.github.com>
2020-05-15 16:43:51 -0400

    WORKIN BROTHERERREESS

---
+++ Gander Horror/Assets/Scripts/Dans/Equations.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Equations
+{
+    /// <summary>
+    /// Rotate towards point
+    /// </summary>
+    /// <param name="destination"></param>
+    /// <param name="objectToRotate"></param>
+    /// <param name="turnSpeed"></param>
+    /// <returns></returns>
+    public static Quaternion RotateTowardsObj(Vector3 destination, Transform objectToRotate, float turnSpeed)
+    {
+        Quaternion newAngle = new Quaternion();
+
+        Vector3 targetDirection = (destination - objectToRotate.transform.position).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        newAngle = Quaternion.RotateTowards(objectToRotate.transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+        newAngle = Quaternion.Euler(new Vector3(0, newAngle.eulerAngles.y, 0));
+
+        return newAngle;
+    }
+
+    /// <summary>
+    /// Get angle between where "viewpointActor" is looking, and a second point
+    /// </summary>
+    /// <param name="destination"></param>
+    /// <param name="viewpointActor"></param>
+    /// <returns></returns>
+    public static float GetAngleBetweenTwoPoints(Vector3 destination, Transform viewpointActor)
+    {
+        Vector3 targetDir = destination - viewpointActor.transform.position;
+        float angle = Vector3.Angle(targetDir, viewpointActor.transform.forward);
+
+        return angle;
+    }
+
+    /*
+    public static Node[] GetQuickestPathToLocation(Transform actor, Node startingNode, Node.Locations location)
+    {
+        Node[] path;
+
+        Stack<Node> order = new Stack<Node>();
+        startingNode.SetVisited(true);
+        order.Push(startingNode);
+        float shortestPath;
+
+        while (order.Peek().CheckForLocation(location) != true || order.Count == 0)
+        {
+            Node newNode = null;
+            Node[] newNeighbors = order.Peek().neighbors;
+
+            for(int i = 0; i < newNeighbors.Length; i++)
+            {
+                if(newNeighbors[i].CheckIfVisited() == false)
+                {
+                    newNode = newNeighbors[i];
+                    newNeighbors[i].SetVisited(true);
+                    break;
+                }
+            }
+
+            if(newNode != null)
+            {
+                order.Push(newNode);
+            }
+        }
+
+
+
+
+        return path;
+    }
+    */
+}
---
+++ Gander Horror/Assets/Scripts/Dans/WaypointNavigation.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointNavigation : MonoBehaviour
+{
+    [SerializeField]
+    private Node currentNode;
+    private Vector3 currentDestination;
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    [Range(0, 10)]
+    private float walkSpeed;
+    [SerializeField]
+    [Range(0, 10)]
+    private float runSpeed;
+    [SerializeField]
+    [Range(0, 1000)]
+    private float turnSpeed;
+    [Header("")]
+    [SerializeField]
+    [Range(0, 180)]
+    private float maxTurnRotation;
+    [SerializeField]
+    [Range(0, 1)]
+    private float minDistanceToDestination;
+    [SerializeField]
+    [Range(0, 100)]
+    [Tooltip("This is the percentage chance of the goose not choosing the node it just came from")]
+    private float chanceForNewNode;
+    private Node lastNode = null;
+
+    [SerializeField]
+    [Tooltip("The max amount of time the goose waits before moving to a new node")]
+    private float maxPotentiaGooseWaitTime;
+
+    private bool aggro = false;
+    private bool movingTowardsDestination = true;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentDestination = currentNode.GetDestination();
+    }
+
+    void Update()
+    {
+        //Rotate towards destination
+        if (movingTowardsDestination)
+        {
+            transform.rotation = Equations.RotateTowardsObj(currentDestination, this.transform, turnSpeed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        //go to node
+        if (movingTowardsDestination)
+        {
+            float movementSpeed = aggro ? runSpeed : walkSpeed;
+            rb.MovePosition((transform.position + (transform.forward * movementSpeed * Time.deltaTime)));
+
+            //if goose reaches its destination
+            //if go to next destination if the current angle to the new destination exceeds var max turn rotation, the goose will turn before moving to next destination
+            if (Vector3.Distance(currentDestination, transform.position) < minDistanceToDestination)
+            {
+                movingTowardsDestination = false;
+
+                //Random: Go immediately to next destination or wait a bit
+                int randNum = Random.Range(0, 2);
+                switch (randNum)
+                {
+                    //go immediately
+                    case 0:
+                        FindNewNode();
+                        break;
+
+                    //wait a tad
+                    case 1:
+                        StartCoroutine(GooseWaitTime());
+                        break;
+                }
+            }
+        }
+
+        //random chance for pause and look around, also possible redirection of node
+    }
+
+    private void FindNewNode()
+    {
+        Node tempNode = currentNode;
+        currentNode = currentNode.GetNewNeighbor(lastNode, chanceForNewNode);
+        lastNode = tempNode;
+        currentDestination = currentNode.GetDestination();
+
+        //turn goose before moving if necessary
+        StartCoroutine(GooseTurnBeforeMoving());
+    }
+
+    private IEnumerator GooseTurnBeforeMoving()
+    {
+        float angle = Equations.GetAngleBetweenTwoPoints(currentDestination, this.transform);
+
+        while (angle > maxTurnRotation)
+        {
+            transform.rotation = Equations.RotateTowardsObj(currentDestination, this.transform, turnSpeed);
+            angle = Equations.GetAngleBetweenTwoPoints(currentDestination, this.transform);
+            yield return null;
+        }
+
+        movingTowardsDestination = true;
+    }
+
+    private IEnumerator GooseWaitTime()
+    {
+        yield return new WaitForSeconds(Random.Range(1, maxPotentiaGooseWaitTime));
+        FindNewNode();
+    }
+
+    //stop and reconsider movement ? wait and go back to path : WALK to a node nearby the noise heard
+    //For when player makes minor noise
+    public void ReconsiderMovement(Node destination)
+    {
+
+    }
+
+    //When player makes a loud noise forget current destination and RUN to the node nearest the noise made, starts walking when he reaches point
+    //-very brief pause tho
+    public void HurryToPlayer(Node Destination)
+    {
+
+    }
+}
+
